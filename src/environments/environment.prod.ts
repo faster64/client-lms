0a58ddb972acc23e16a54ee2b4d0ebbba6b60bb6
Cuong <faster64>
2024-01-22 00:02:02 +0700

    #

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   organization: "lms",
   api_version: "v1.0",
-  base_host: "https://corej2.com/api",
-  hub_url: "https://corej2.com/socket-message",
+  base_host: "https://develop.sp.io.vn/api",
+  hub_url: "https://develop.sp.io.vn/socket-message",
   default_language: 'vi-VN'
 };
