44338aa6ca3000168dde8b7efeb728eb62162518
Cuong <faster64>
2024-01-22 00:45:41 +0700

    #

--- src/app/shared/guards/unauthenticated-only.guard.ts
+++ src/app/shared/guards/unauthenticated-only.guard.ts
@@ -1,13 +1,23 @@
 import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
 import { Observable } from "rxjs";
+import { AuthService } from "../services/auth/auth.service";
+import { AuthStatus } from "../enums/auth-status.enum";
+import { Routing } from "../constants/routing.constant";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UnauthenticatedOnlyGuard implements CanActivate {
 
+  constructor(public router: Router) {
+  }
+
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    if (AuthService.CurrentStatus == AuthStatus.LoggedIn) {
+      this.router.navigateByUrl(Routing.HOME.path);
+      return false;
+    }
     return true;
   }
 }
