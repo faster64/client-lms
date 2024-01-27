84aef93a755729fa1e3beab74ed5912447f61c8c
Cuong <faster64>
2024-01-27 14:07:18 +0700

    #

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   organization: "lms",
   api_version: "v1.0",
-  base_host: "https://develop.sp.io.vn/api",
-  hub_url: "https://develop.sp.io.vn/socket-message",
+  base_host: "https://open-vn.pro/api",
+  hub_url: "https://open-vn.pro/socket-message",
   upload_url: "https://kernellocker.io.vn",
   default_language: 'vi-VN'
 };
