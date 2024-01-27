cdd5839f75ccd9d3a37c05811276b510a170ae7d
Cuong <faster64>
2024-01-27 21:12:49 +0700

    #

--- src/app/cms/cms-page/cms-lesson/cms-lesson.component.ts
+++ src/app/cms/cms-page/cms-lesson/cms-lesson.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import { Course } from 'src/app/shared/models/course/course';
+import { CmsGridComponent } from '../cms-page-grid.component';
 
 @Component({
   selector: 'app-cms-lesson',
   templateUrl: './cms-lesson.component.html',
   styleUrls: ['./cms-lesson.component.scss']
 })
-export class CmsLessonComponent {
+export class CmsLessonComponent extends CmsGridComponent<Course> {
 
+  // override ngOnInit(): void {
+  //   this.injector.get(PublisherService).updateCmsHeaderLabelEvent.emit('Quản lý Khóa học');
+  //   super.ngOnInit();
+  // }
+
+  // override initConfig(): void {
+  //   this.service = this.injector.get(CourseService);
+  // }
+
+  // override initColumns() {
+  //   this.displayColumns = [];
+  //   this.displayColumns.push({ column: 'imageUrl', displayText: 'Hình ảnh', width: 120, type: FieldType.Image });
+  //   this.displayColumns.push({ column: 'code', displayText: 'Mã khóa học', width: 160 });
+  //   this.displayColumns.push({ column: 'name', displayText: 'Tên khóa học', width: 300 });
+  //   this.displayColumns.push({ column: 'shortDescription', displayText: 'Mô tả ngắn', width: 480 });
+  //   this.displayColumns.push({ column: 'className', displayText: 'Lớp', width: 120 });
+  //   this.displayColumns.push({ column: 'price', displayText: 'Giá', width: 140, type: FieldType.Number });
+  //   this.displayColumns.push({ column: 'statusText', displayText: 'Trạng thái', width: 160 });
+  // }
+
+  // override filterResponse = (data) => {
+  //   for (let i = 0; i < data.length; i++) {
+  //     const item = data[i];
+  //     const className = item.status == CourseStatus.Release ? 'release' : item.status == CourseStatus.CommingSoon ? 'comming-soon' : 'inactive';
+  //     item.statusText = `<span class='${className}'>${item.statusText}</span>`;
+  //   }
+
+  //   return data;
+  // }
 }
