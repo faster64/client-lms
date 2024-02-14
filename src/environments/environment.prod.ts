3cd2cf2f0f5b0f67d75649a493b2dbba58b5a82d
Cuong <faster64>
2024-02-14 22:38:20 +0700

    #

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   organization: "lms",
   api_version: "v1.0",
-  base_host: "https://open-vn.pro/api",
-  hub_url: "https://open-vn.pro/socket-message",
+  base_host: "https://api-lms.chaien.vn/api",
+  hub_url: "https://api-lms.chaien.vn/socket-message",
   upload_url: "https://content.chaien.vn",
   default_language: 'vi-VN'
 };
