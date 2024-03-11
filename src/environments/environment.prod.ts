7d04f1d1b75d73e88a08e8cee89976e9805fdbdf
cuong.nv3@kiotviet.com <cuong.nv3@kiotviet.com>
2024-03-11 09:32:57 +0700

    #

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   organization: "lms",
   api_version: "v1.0",
-  base_host: "https://ap-southeast-dm-1.chaien.vn/api",
-  hub_url: "https://ap-southeast-dm-1.chaien.vn/socket-message",
+  base_host: "https://ap-southeast-high-dm-1.chaien.vn/api",
+  hub_url: "https://ap-southeast-high-dm-1.chaien.vn/socket-message",
   upload_url: "https://cdn.chaien.vn",
   default_language: 'vi-VN',
   isDemo: true
