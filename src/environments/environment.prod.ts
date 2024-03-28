8da018a28a3afb20d2c2d8418c7a2b7c98dfe6b2
Cuong <faster64>
2024-03-28 20:25:25 +0700

    #

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   organization: "lms",
   api_version: "v1.0",
-  base_host: "https://ap-southeast-dm-1.chaien.vn/api",
-  hub_url: "https://ap-southeast-dm-1.chaien.vn/socket-message",
+  base_host: "https://cab.oapi.vn/api",
+  hub_url: "https://cab.oapi.vn/socket-message",
   upload_url: "https://cdn.chaien.vn",
   default_language: 'vi-VN',
   isDemo: true
